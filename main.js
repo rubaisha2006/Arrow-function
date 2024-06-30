7f2fd4d582c150fbe205e51d3f996b5f73a8c63d
rubaisha2006 <rubaishaaltaf@outlook.com>
2024-06-30 11:42:57 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var problem = function () { return 5 + 5; };
+var call = problem();
+console.log(call);
+var Name = function () { return "I am Rubaisha"; };
+var calling = Name();
+console.log(calling);
---
+++ main.ts
@@ -0,0 +1,7 @@
+let problem = () => 5+5;
+let call = problem();
+console.log(call);
+
+let Name = () => "I am Rubaisha";
+let calling = Name();
+console.log(calling); 
